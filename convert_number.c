47d53f71ffae6007ae7255b83dfcbffe1d9561fe
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-09-17 13:19:38 -0700

    convert numbers

---
+++ convert_number.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * print_hex - Prints a hexadecimal number.
+ * @ap: The argument list.
+ * @params: The printing parameters.
+ * Return: Number of characters printed.
+ */
+int print_hex(va_list ap, params_t *params)
+{
+	unsigned long l;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned short int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'x';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (print_number(str, params));
+}
+
+/**
+ * print_HEX - Prints a hexadecimal number in uppercase.
+ * @ap: The argument list.
+ * @params: The printing parameters.
+ * Return: Number of characters printed.
+ */
+int print_HEX(va_list ap, params_t *params)
+{
+	unsigned long l;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned short int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (print_number(str, params));
+}
+
+/**
+ * print_binary - Prints a number in binary.
+ * @ap: The argument list.
+ * @params: The printing parameters.
+ * Return: Number of characters printed.
+ */
+int print_binary(va_list ap, params_t *params)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+
+	if (params->hashtag_flag && n)
+		*--str = '0';
+
+	params->unsign = 1;
+	return (print_number(str, params));
+}
+
+/**
+ * print_octal - Prints a number in octal.
+ * @ap: The argument list.
+ * @params: The printing parameters.
+ * Return: Number of characters printed.
+ */
+int print_octal(va_list ap, params_t *params)
+{
+	unsigned long l;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned short int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 8, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+		*--str = '0';
+
+	params->unsign = 1;
+	return (print_number(str, params));
+}
