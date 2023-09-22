f23c3b1fd6afc7171b52e6b08d584be5c6fd0bb1
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-09-22 08:40:45 -0700

    printf

--- main.h
+++ main.h
@@ -5,4 +5,14 @@ int _printf(const char *format, ...);
 int _strlen(char *s);
 int print_number(int num);
 void _itoa(int num, char *str);
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 #endif /* MAIN_H */
